931e6a1bbc9b4a65fb0cc3334078ebd25d7985c0
Vivek Kumar Brohma <vivek.brohma@myntra.com>
2016-06-27 00:12:01 +0530

    Finalizing the react-node app

---
+++ lib/react/pages/Index.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Index = React.createClass({
+	render: function () {
+		return (
+			<div className="index">
+				Home
+			</div>
+		);
+	}
+});
+
+module.exports = Index;
